6c9463358b2174d3d8bdcd4fce2a8b98df0dc37e
YasirGH <y.elgharnaji@gmail.com>
2023-09-29 20:12:49 +0200

    Creating API to Get All Product Details

--- src/main/java/com/youtube/ecommerce/controller/ProductController.java
+++ src/main/java/com/youtube/ecommerce/controller/ProductController.java
@@ -6,6 +6,7 @@ import com.youtube.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -47,5 +49,10 @@ public class ProductController {
             imageModels.add(imageModel);
         }
         return imageModels;
+    }
+
+    @GetMapping(value = {"/getAllProducts"})
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
