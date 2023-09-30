c9765b461d72ddb085eb77e2f99ad4d9343f3417
YasirGH <y.elgharnaji@gmail.com>
2023-09-30 20:53:39 +0200

    delete product endpoint

--- src/main/java/com/youtube/ecommerce/controller/ProductController.java
+++ src/main/java/com/youtube/ecommerce/controller/ProductController.java
@@ -6,10 +6,7 @@ import com.youtube.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -54,5 +51,10 @@ public class ProductController {
     @GetMapping("/getAllProducts")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @DeleteMapping("/deleteProductDetails/{productId}")
+    public void deleteProduct(@PathVariable("productId") Integer productId) {
+        productService.deleteProductDetails(productId);
     }
 }
