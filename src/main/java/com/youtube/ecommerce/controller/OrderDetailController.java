97f03722c1b3ec94875fb0523f19a18c315f017a
YasirGH <y.elgharnaji@gmail.com>
2023-10-03 13:03:22 +0200

    Creating API to Place Order

---
+++ src/main/java/com/youtube/ecommerce/controller/OrderDetailController.java
@@ -0,0 +1,23 @@
+package com.youtube.ecommerce.controller;
+
+import com.youtube.ecommerce.entity.OrderInput;
+import com.youtube.ecommerce.service.OrderDetailsService;
+import com.youtube.ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class OrderDetailController {
+
+    @Autowired
+    private OrderDetailsService orderDetailsService;
+
+    @PreAuthorize("hasRole('User')")
+    @PostMapping({"/placeOrder"})
+    public void placeOrder(@RequestBody OrderInput orderInput) {
+        orderDetailsService.placeOrder(orderInput);
+    }
+}
---
+++ src/main/java/com/youtube/ecommerce/service/OrderDetailsService.java
@@ -0,0 +1,45 @@
+package com.youtube.ecommerce.service;
+
+import com.youtube.ecommerce.configuration.JwtRequestFilter;
+import com.youtube.ecommerce.dao.OrderDetailDao;
+import com.youtube.ecommerce.dao.ProductDao;
+import com.youtube.ecommerce.dao.UserDao;
+import com.youtube.ecommerce.entity.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderDetailsService {
+
+    private final static String ORDER_PLACED = "Placed";
+
+    @Autowired
+    private OrderDetailDao orderDetailsDao;
+    @Autowired
+    private ProductDao productDao;
+
+    @Autowired
+    private UserDao userDao;
+
+    public void placeOrder(OrderInput orderInput) {
+        List<OrderProductQuantity> productQuantityList = orderInput.getOrderProductQuantityList();
+        for (OrderProductQuantity o : productQuantityList) {
+            Product product = productDao.findById(o.getProductId()).get();
+            String currentUser = JwtRequestFilter.CURRENT_USER;
+            User user = userDao.findById(currentUser).get();
+            OrderDetail orderDetail = new OrderDetail(
+                    orderInput.getFullName(),
+                    orderInput.getFullAddress(),
+                    orderInput.getContactNumber(),
+                    orderInput.getAlternateContactNumber(),
+                    ORDER_PLACED,
+                    product.getProductActualPrice() * o.getQuantity(),
+                    product,
+                    user
+            );
+            orderDetailsDao.save(orderDetail);
+        }
+    }
+}
