738b1f3dbbd3505a1b04ca53b97d0ee74ca3ff64
YasirGH <y.elgharnaji@gmail.com>
2023-10-06 22:41:42 +0200

    deleting cart item

--- src/main/java/com/youtube/ecommerce/controller/CartController.java
+++ src/main/java/com/youtube/ecommerce/controller/CartController.java
@@ -4,6 +4,7 @@ import com.youtube.ecommerce.entity.Cart;
 import com.youtube.ecommerce.service.CartService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,8 +25,14 @@ public class CartController {
     }
 
     @PreAuthorize("hasRole('User')")
+    @DeleteMapping({"/deleteCartItem/{cartId}"})
+    public void deleteCartItem(@PathVariable(name = "cartId") Integer cartId) {
+        cartService.deleteCartItem(cartId);
+    }
+
+    @PreAuthorize("hasRole('User')")
     @GetMapping({"/getCartDetails"})
-    public List<Cart> getCartDetails(){
+    public List<Cart> getCartDetails() {
         return cartService.getCartDetails();
     }
 }
