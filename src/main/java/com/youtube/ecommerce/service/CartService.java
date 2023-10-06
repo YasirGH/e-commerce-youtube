462a4828e6e395ffa5455d14f5b7adfdf79c6796
YasirGH <y.elgharnaji@gmail.com>
2023-10-06 20:03:21 +0200

    Creating API to get Cart Details

--- src/main/java/com/youtube/ecommerce/service/CartService.java
+++ src/main/java/com/youtube/ecommerce/service/CartService.java
@@ -10,6 +10,8 @@ import com.youtube.ecommerce.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CartService {
 
@@ -35,6 +37,12 @@ public class CartService {
             return cartDao.save(cart);
         }
         return null;
+    }
+
+    public List<Cart> getCartDetails() {
+        String username = JwtRequestFilter.CURRENT_USER;
+        User user = userDao.findById(username).get();
+        return cartDao.findByUser(user);
     }
 
 }
